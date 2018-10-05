f52a3ac404653cb6fe788019d1836bf3805d551f
MarkSherstan <mshersta@ualberta.ca>
2018-10-04 21:09:53 -0600

    Increase sampling rate to ~150 Hz

--- Accelerometer/accelerometer4g/accelerometer4g.ino
+++ Accelerometer/accelerometer4g/accelerometer4g.ino
@@ -12,47 +12,49 @@ void setup() {
 
   //Setup the registers of the MPU-6050 (500dfs / +/-2g) and start up
   setup_mpu_6050_registers();
-
+  
   //Reset the loop timer
   loop_timer = micros();
 }
 
 
-void loop(){
-    //Read the raw acc data from MPU-6050
-    read_mpu_6050_data();
+void loop() {
+  //Read the raw acc data from MPU-6050
+  read_mpu_6050_data();
 
-    // Convert to g force
-    accel_x = (double)acc_x / (double)scaleFactor;
-    accel_y = (double)acc_y / (double)scaleFactor;
-    accel_z = (double)acc_z / (double)scaleFactor;
+  // Convert to g force
+  accel_x = (double)acc_x / (double)scaleFactor;
+  accel_y = (double)acc_y / (double)scaleFactor;
+  accel_z = (double)acc_z / (double)scaleFactor;
 
-    // Print / write data
-    Serial.print("X: "); Serial.print(accel_x,10); Serial.print("  ");
-    Serial.print("Y: "); Serial.print(accel_y,10); Serial.print("  ");
-    Serial.print("Z: "); Serial.print(accel_z,10); Serial.print("  ");
-    Serial.print("Frequency: [Hz] "); Serial.println(1e6/(micros() - loop_timer));
+  // Print / write data
+  Serial.print(loop_timer); Serial.print(",");
+  Serial.print(accel_x, 7); Serial.print(",");
+  Serial.print(accel_y, 7); Serial.print(",");
+  Serial.println(accel_z, 7);
 
-    // Wait until the loop_timer reaches 4000us (250Hz) before starting the next loop
-    while(micros() - loop_timer < 4000);
-    loop_timer = micros();
+  // Wait until the loop_timer reaches 4000us (250Hz) before starting the next loop
+  while (micros() - loop_timer < 4000);
+  loop_timer = micros();
 }
 
-void read_mpu_6050_data(){
+
+void read_mpu_6050_data() {
   //Subroutine for reading the raw accelerometer data
   Wire.beginTransmission(0x68);                                        //Start communicating with the MPU-6050
   Wire.write(0x3B);                                                    //Send the requested starting register
   Wire.endTransmission();                                              //End the transmission
-  Wire.requestFrom(0x68,6);                                            //Request 6 bytes from the MPU-6050
+  Wire.requestFrom(0x68, 6);                                           //Request 6 bytes from the MPU-6050
 
   // Read data
-  while(Wire.available() < 6);                                         //Wait until all the bytes are received
-  acc_x = Wire.read()<<8|Wire.read();                                  //Add the low and high byte to the acc_x variable
-  acc_y = Wire.read()<<8|Wire.read();                                  //Add the low and high byte to the acc_y variable
-  acc_z = Wire.read()<<8|Wire.read();                                  //Add the low and high byte to the acc_z variable
+  while (Wire.available() < 6);                                        //Wait until all the bytes are received
+  acc_x = Wire.read() << 8 | Wire.read();                              //Add the low and high byte to the acc_x variable
+  acc_y = Wire.read() << 8 | Wire.read();                              //Add the low and high byte to the acc_y variable
+  acc_z = Wire.read() << 8 | Wire.read();                              //Add the low and high byte to the acc_z variable
 }
 
-void setup_mpu_6050_registers(){
+
+void setup_mpu_6050_registers() {
   //Activate the MPU-6050
   Wire.beginTransmission(0x68);                                        //Start communicating with the MPU-6050
   Wire.write(0x6B);                                                    //Send the requested starting register
