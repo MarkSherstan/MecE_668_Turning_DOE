ba78659a183bd64dd05f41bf11fa1c8f669d85f5
Mark Sherstan <mshersta@ualberta.ca>
2018-10-05 20:05:21 -0600

    Change from dlmread to csvread

--- dataLogging/txtReader.m
+++ dataLogging/txtReader.m
@@ -2,13 +2,14 @@ clear all
 close all
 clc
 
-master = dlmread('TEST.TXT');
+filename = 'DATA00.CSV';
+M = csvread(filename);
 
-loop_timer_micro = master(:,1);
+loop_timer_micro = M(:,1);
 loop_timer_seconds = loop_timer_micro * 1e-6;
-accel_x = master(:,2);
-accel_y = master(:,3);
-accel_z = master(:,4);
+accel_x = M(:,2);
+accel_y = M(:,3);
+accel_z = M(:,4);
 
 for i = 1:length(loop_timer_micro)-1
   frequency(i) = loop_timer_micro(i+1) - loop_timer_micro(i);
