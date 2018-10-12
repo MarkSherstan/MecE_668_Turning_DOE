d2ed827b0a7cad476a40aafc7ddbceafb612a537
MarkSherstan <mshersta@ualberta.ca>
2018-10-12 08:41:27 -0600

    Added scale to main and removed from V logger

--- MATLAB_DAQ/main.m
+++ MATLAB_DAQ/main.m
@@ -8,7 +8,15 @@ dataFileName = 'DATA01.csv';
 extraPoints = 50;
 
 tic
-[vel, pos] = velocityLogger(videoFileName,extraPoints);
+% [scale, red, blue, green] = scale(filename,false);
+% 30 fps --> 0.029464206496742
+% 240 fps --> 0.048056789348528
+scale = 0.048056789348528;
+toc
+
+
+tic
+[vel, pos] = velocityLogger(videoFileName,extraPoints,scale);
 toc
 
 
