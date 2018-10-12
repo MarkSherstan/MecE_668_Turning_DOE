907e437992d232aa943398118ae905be39a1911e
MarkSherstan <mshersta@ualberta.ca>
2018-10-11 23:26:16 -0600

    Filter out unused data at start of sampling.

--- dataLogging/fileReader.m
+++ dataLogging/fileReader.m
@@ -14,8 +14,22 @@ function [a,w,t] = fileReader(filename,flag)
   w.z = M(:,7);
 
   for i = 1:length(t.microSeconds)-1
-    t.frequency(i) = 1e6 / (t.microSeconds(i+1) - t.microSeconds(i));
+    t.frequency(i,1) = 1e6 / (t.microSeconds(i+1) - t.microSeconds(i));
   end
+
+  mag = sqrt(a.x.^2 + a.y.^2 + a.z.^2);
+  sliceLocation = dataSlicer(mag,50); % 50 Extra points
+
+  t.microSeconds(1:sliceLocation) = [];
+  t.seconds(1:sliceLocation) = [];
+  t.frequency(1:sliceLocation) = [];
+
+  a.x(1:sliceLocation) = [];
+  a.y(1:sliceLocation) = [];
+  a.z(1:sliceLocation) = [];
+  w.x(1:sliceLocation) = [];
+  w.y(1:sliceLocation) = [];
+  w.z(1:sliceLocation) = [];
 
   if flag == true
     figure(1)
@@ -49,4 +63,20 @@ function [a,w,t] = fileReader(filename,flag)
     Leg = legend([a; b], {'a_x','a_y','a_z','w_x','w_y','w_z'});
   end
 
+end
+
+function [sliceLocation] = dataSlicer(mag,extraPoints)
+  M2 = movmean(mag,2);
+  M4 = movmean(mag,4);
+
+  counter = 0;
+
+  for i = 1:length(mag)
+    if abs((M2(i) - M4(i)) / (M4(i))) > 0.1
+      break
+    end
+    counter = counter + 1;
+  end
+
+  sliceLocation = counter - extraPoints;
 end
