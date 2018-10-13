1810111ce411cd40ed37c1a9519e76c0cf884037
MarkSherstan <mshersta@ualberta.ca>
2018-10-13 15:37:49 -0600

    Converted angles to cycle 2*pi

--- MATLAB_DAQ/velocityLogger.m
+++ MATLAB_DAQ/velocityLogger.m
@@ -1,17 +1,16 @@
-function [angularV, instantV, pos] = velocityLogger(filename,extraPoints,scale)
+function [angleRad, angleDeltaRad, pos] = velocityLogger(filename,extraPoints,scale)
 
-  % Declare variables
-  i = 1;
-
-  % Read in video and process
+  % Read in video and process and initialize counter
   v = VideoReader(filename);
   frameRate = v.FrameRate;
+  i = 1;
 
   % Loop through video frame by frame
   while hasFrame(v)
       frame = readFrame(v);
       BW = createMask(frame);
 
+      % Find centroid x y coords
       stats = regionprops('table',BW,'Centroid');
       points = table2array(stats);
 
@@ -19,42 +18,54 @@ function [angularV, instantV, pos] = velocityLogger(filename,extraPoints,scale)
       pos.x(i) = points(1);
       pos.y(i) = points(2);
 
-      % Display video if required
-      %figure(10)
-      %imshow(frame)
-      %imshow(BW)
-
-      % Counter
+      % Counter and print
       i = i + 1;
       fprintf('%d\n',i)
   end
 
   % Calculate angular and instantaneous velocity
-  [angularV, instantV] = variousVCalc(pos,frameRate,scale);
+  [angleRad, angleDeltaRad] = variousVCalc(pos,frameRate,scale);
 
   % Get rid of usless data
-  sliceLocation = dataSlicer(angularV,extraPoints);
-  angularV(1:sliceLocation) = [];
-  instantV(1:sliceLocation) = [];
-  pos.x(1:sliceLocation) = [];
-  pos.y(1:sliceLocation) = [];
+  % sliceLocation = dataSlicer(angularV,extraPoints);
+  % angularV(1:sliceLocation) = [];
+  % instantV(1:sliceLocation) = [];
+  % pos.x(1:sliceLocation) = [];
+  % pos.y(1:sliceLocation) = [];
 
 end
 
 
-function [angularV, instantV] = variousVCalc(pos,frameRate,scale)
+function [angleRad, angleDeltaRad] = variousVCalc(pos,frameRate,scale)
   % Center data at 0
   centerX = mean(pos.x);
   centerY = mean(pos.y);
   positionX = pos.x - centerX;
   positionY = pos.y - centerY;
 
-  for i = 1:length(positionX)-1
-    % angle/frame * frame/seconds * cm/pixel --> Is this correct???
-    angularV(i) = (tan(positionY(i+1)/positionX(i+1)) - tan(positionY(i)/positionX(i)))/frameRate;
+  % Convert to [0 2*pi]
+  for i = 1:length(positionX)
+    angleRad(i) = atan2(positionY(i),positionX(i));
 
-    % pixels/frame * frame/seconds * cm/pixel --> Is this correct???
-    instantV(i) = angularV(i) * sqrt(positionX(i)^2 + positionY(i)^2);
+    if (angleRad(i) <= 0) && (angleRad(i) >= -pi)
+      angleRad(i) = angleRad(i) + 2*pi;
+    end
+  end
+
+  % Calculate change in angle
+  for i = 1:length(angleRad) - 1
+    angleDeltaRad(i) = angleRad(i+1) - angleRad(i);
+
+    % if clockwise
+    % if angleDeltaRad(i) < 0
+    %   angleDeltaRad(i) = angleDeltaRad(i) + 2*pi;
+    % end
+
+    % if counter clockwise
+    if angleDeltaRad(i) > 0
+      angleDeltaRad(i) = angleDeltaRad(i) - 2*pi;
+    end
+
   end
 
 end
@@ -64,7 +75,7 @@ function [sliceLocation] = dataSlicer(angularV,extraPoints)
   counter = 0;
 
   for i = 1:length(angularV)
-    if angularV(i) > 1.5
+    if angularV(i) > 0.01
       break
     end
 
