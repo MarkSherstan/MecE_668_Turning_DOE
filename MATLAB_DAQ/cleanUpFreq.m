c3a08682f7547b00486266a281717a1d3a4248d2
MarkSherstan <mshersta@ualberta.ca>
2018-10-13 15:34:14 -0600

    Added comments and cleaned up formatting

--- MATLAB_DAQ/cleanUpFreq.m
+++ MATLAB_DAQ/cleanUpFreq.m
@@ -1,40 +1,36 @@
-function [] = cleanUpFreq(acc,freq)
-% fileName = 'DATA01.CSV';
-% [a,w,t] = fileReader(fileName,-50);
-% acc = a.y(1:800);
-% freq = mean(t.frequency);
-% cleanUp(acc,freq)
+function [dataOut] = cleanUpFreq(acc,freq)
 
-Fs = freq;            % Sampling frequency
-T = 1/Fs;             % Sampling period
-L = length(acc);      % Length of signal
-t = (0:L-1)*T;        % Time vector
+% Set up starting variables
+Fs = freq;
+T = 1/Fs;
+L = length(acc);
+t = (0:L-1)*T;
+
+% Plot original data in time domain
 figure(1)
 plot(freq*t,acc)
 
+% Perfom FFT
 Y = fft(acc);
 P2 = abs(Y/L);
 P1 = P2(1:L/2+1);
 P1(2:end-1) = 2*P1(2:end-1);
-
 f = Fs*(0:(L/2))/L;
+
+% Plot frequency domain
 figure(2)
 plot(f,P1)
 title('Single-Sided Amplitude Spectrum of X(t)')
 xlabel('f (Hz)')
 ylabel('|P1(f)|')
 
-
-%%%
-
-fc = 0.5; % Change this value
+% Design a 6th order butterworth filter based off the frequnecy domain plot
+fc = 0.5; %                                <----              Change this value
 fs = freq;
-
 [b,a] = butter(6,fc/(fs/2));
 
 dataOut = filter(b,a,acc);
+
+% Plot the filtered data back in the time domain
 figure(4)
 plot(dataOut)
-
-
-return
