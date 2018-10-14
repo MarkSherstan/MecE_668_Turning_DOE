47472fbce06f7f838e11e5b86e6f501960c9e318
MarkSherstan <mshersta@ualberta.ca>
2018-10-13 21:38:42 -0600

    Remove velocity calc and data slicer.

--- MATLAB_DAQ/velocityLogger.m
+++ MATLAB_DAQ/velocityLogger.m
@@ -1,5 +1,5 @@
-function [angleRad, angleDeltaRad, pos] = velocityLogger(filename,extraPoints,scale)
-
+function [pos] = positionLogger(filename, extraPoints)
+  tic
   % Read in video and process and initialize counter
   v = VideoReader(filename);
   frameRate = v.FrameRate;
@@ -22,65 +22,5 @@ function [angleRad, angleDeltaRad, pos] = velocityLogger(filename,extraPoints,sc
       i = i + 1;
       fprintf('%d\n',i)
   end
-
-  % Calculate angular and instantaneous velocity
-  [angleRad, angleDeltaRad] = variousVCalc(pos,frameRate,scale);
-
-  % Get rid of usless data
-  % sliceLocation = dataSlicer(angularV,extraPoints);
-  % angularV(1:sliceLocation) = [];
-  % instantV(1:sliceLocation) = [];
-  % pos.x(1:sliceLocation) = [];
-  % pos.y(1:sliceLocation) = [];
-
-end
-
-
-function [angleRad, angleDeltaRad] = variousVCalc(pos,frameRate,scale)
-  % Center data at 0
-  centerX = mean(pos.x);
-  centerY = mean(pos.y);
-  positionX = pos.x - centerX;
-  positionY = pos.y - centerY;
-
-  % Convert to [0 2*pi]
-  for i = 1:length(positionX)
-    angleRad(i) = atan2(positionY(i),positionX(i));
-
-    if (angleRad(i) <= 0) && (angleRad(i) >= -pi)
-      angleRad(i) = angleRad(i) + 2*pi;
-    end
-  end
-
-  % Calculate change in angle
-  for i = 1:length(angleRad) - 1
-    angleDeltaRad(i) = angleRad(i+1) - angleRad(i);
-
-    % if clockwise
-    % if angleDeltaRad(i) < 0
-    %   angleDeltaRad(i) = angleDeltaRad(i) + 2*pi;
-    % end
-
-    % if counter clockwise
-    if angleDeltaRad(i) > 0
-      angleDeltaRad(i) = angleDeltaRad(i) - 2*pi;
-    end
-
-  end
-
-end
-
-
-function [sliceLocation] = dataSlicer(angularV,extraPoints)
-  counter = 0;
-
-  for i = 1:length(angularV)
-    if angularV(i) > 0.01
-      break
-    end
-
-    counter = counter + 1;
-  end
-
-  sliceLocation = counter - extraPoints;
+  toc
 end
