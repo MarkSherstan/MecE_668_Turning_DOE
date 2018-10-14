66820191c237956d4fb2208007c73875c4777845
MarkSherstan <mshersta@ualberta.ca>
2018-10-13 22:47:49 -0600

    Create dataSlicer function to clean up other functions

---
+++ MATLAB_DAQ/dataSlicer.m
@@ -0,0 +1,63 @@
+function [a,w,t,pos,pos2] = dataSlicer(a,w,t,pos,extraPoints)
+
+  % Calculate magnitudes and find slice locations
+  magW = sqrt(w.x.^2 + w.y.^2 + w.z.^2);
+  magPos = sqrt(pos.x.^2 + pos.y.^2);
+
+  sliceLocation(1) = sliceSensor(magW, extraPoints);
+  sliceLocation(2) = sliceVideo(magPos, extraPoints);
+
+  % Output sliced a w and t
+  a.x(1:sliceLocation(1)) = [];
+  a.y(1:sliceLocation(1)) = [];
+  a.z(1:sliceLocation(1)) = [];
+  w.x(1:sliceLocation(1)) = [];
+  w.y(1:sliceLocation(1)) = [];
+  w.z(1:sliceLocation(1)) = [];
+  t.microSeconds(1:sliceLocation(1)) = [];
+  t.seconds(1:sliceLocation(1)) = [];
+  t.frequency(1:sliceLocation(1)) = [];
+
+  % Output sliced pos
+  pos.x(1:sliceLocation(2)) = [];
+  pos.y(1:sliceLocation(2)) = [];
+
+  % Resample to pair video and sensor
+  pos2.x = resample(pos.x,length(a.x),length(pos.x));
+  pos2.y = resample(pos.y,length(a.x),length(pos.y));
+end
+
+
+function [sliceLocation] = sliceSensor(mag, extraPoints)
+  % Initialize variables
+  counter = 0;
+  forward = movmean(mag,[0, 2]);
+  back = movmean(mag,[2, 0]);
+
+  % Find the point to cut at
+  for j = 1:length(mag)
+    if abs((forward(j) - back(j)) / (back(j))) > 0.1
+      break
+    end
+    counter = counter + 1;
+  end
+
+  sliceLocation = counter - extraPoints;
+end
+
+
+function [sliceLocation] = sliceVideo(mag, extraPoints)
+  % Initialize variables
+  counter = 0;
+  flat = mean(mag(1:150));
+
+  % Find the point to cut at
+  for j = 1:length(mag)
+    if abs((mag(j) - flat) / (flat)) > 0.005
+      break
+    end
+    counter = counter + 1;
+  end
+
+  sliceLocation = counter - extraPoints;
+end
--- MATLAB_DAQ/fileReader.m
+++ MATLAB_DAQ/fileReader.m
@@ -1,7 +1,4 @@
-function [a,w,t] = fileReader(filename,extraPoints)
-% Feed in filename string and true/false for displaying plots
-% Receive accleration, angular velocity, and time/frequency
-
+function [a,w,t] = fileReader(filename)
   M = csvread(filename);
 
   t.microSeconds = M(:,1);
@@ -17,34 +14,4 @@ function [a,w,t] = fileReader(filename,extraPoints)
     t.frequency(i,1) = 1e6 / (t.microSeconds(i+1) - t.microSeconds(i));
   end
 
-  mag = sqrt(a.x.^2 + a.y.^2 + a.z.^2);
-  sliceLocation = dataSlicer(mag,extraPoints);
-
-  t.microSeconds(1:sliceLocation) = [];
-  t.seconds(1:sliceLocation) = [];
-  t.frequency(1:sliceLocation) = [];
-
-  a.x(1:sliceLocation) = [];
-  a.y(1:sliceLocation) = [];
-  a.z(1:sliceLocation) = [];
-  w.x(1:sliceLocation) = [];
-  w.y(1:sliceLocation) = [];
-  w.z(1:sliceLocation) = [];
-
-end
-
-function [sliceLocation] = dataSlicer(mag,extraPoints)
-  M2 = movmean(mag,2);
-  M4 = movmean(mag,4);
-
-  counter = 0;
-
-  for i = 1:length(mag)
-    if abs((M2(i) - M4(i)) / (M4(i))) > 0.08
-      break
-    end
-    counter = counter + 1;
-  end
-
-  sliceLocation = counter - extraPoints;
 end
