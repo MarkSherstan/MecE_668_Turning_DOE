ba63240fdeda9c1a3d464ff56410be1702905ff1
MarkSherstan <mshersta@ualberta.ca>
2018-10-13 23:10:54 -0600

    Update main to reflect new sub functions

--- MATLAB_DAQ/main.m
+++ MATLAB_DAQ/main.m
@@ -2,32 +2,33 @@ clear all
 close all
 clc
 
-
-videoFileName = 'testMe.mov';
-dataFileName = 'DATA01.csv';
+% Constant values / variables
+videoFileName = 'VID00-converted.mp4';
+dataFileName = 'DATA00.csv';
 extraPoints = 50;
 
-tic
+% Get scale
 % [scale, red, blue, green] = scale(filename,false);
 % 30 fps --> 0.029464206496742
 % 240 fps --> 0.048056789348528
 scale = 0.048056789348528;
-toc
+
+% Calculate positions
+pos = positionLogger(videoFileName);
+
+% Read in sensor data and clean where required
+[a,w,t] = fileReader(dataFileName);
+a.y = cleanUpFreq(a.y,mean(t.frequency),4);
+
+% Match up data
+[a,w,t,pos,pos2] = dataSlicer(a,w,t,pos,extraPoints);
+
+% Calculate angles and velocitys
+[angleDeltaRad, angleRad, p] = angleVelCalc(pos, 1, 1450, 10);
+close all
+
+% Cut off ends of data? --> Find where the car slipped
 
 
-tic
-[angularV, instantV, pos] = velocityLogger(videoFileName,extraPoints,scale);
-toc
-
-
-tic
-[a,w,t] = fileReader(dataFileName,extraPoints);
-toc
-
-
-angularVResample = resample(angularV,length(a.y),length(angularV));
-
-
-tic
-plotter(angularV, instantV, pos, a, w, t, angularVResample)
-toc
+% Plot the results --> Add a resample
+plotter(angleDeltaRad, angleRad, pos, a, w, t, p)
