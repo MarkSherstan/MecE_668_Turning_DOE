a557a34f458eedaff2b0c7711fc340cdf411f858
MarkSherstan <mshersta@ualberta.ca>
2018-10-14 16:13:29 -0600

    Updated slicing and have user confirm locations

--- MATLAB_DAQ/dataSlicer.m
+++ MATLAB_DAQ/dataSlicer.m
@@ -1,60 +1,98 @@
-function [a,w,t,pos] = dataSlicer(a,w,t,pos,extraPoints)
+function [a,w,t,pos] = dataSlicer(a,w,t,pos)
 
   % Calculate magnitudes and find slice locations
   magW = sqrt(w.x.^2 + w.y.^2 + w.z.^2);
   magPos = sqrt(pos.x.^2 + pos.y.^2);
 
-  sliceLocation(1) = sliceSensor(magW, extraPoints);
-  sliceLocation(2) = sliceVideo(magPos, extraPoints);
+  sliceLocationSensor = slicer(magW);
+  sliceLocationVideo = slicer(magPos);
+
+  % Get input from user if values are good
+  x = 0;
+  while (x ~= 1)
+    close all
+    figure(1)
+    x1 = 1:length(magPos);
+    x2 = 1:length(magW);
+
+    subplot(2,1,1)
+    hold on
+    plot(x2,magW,'--m')
+    plot([sliceLocationSensor(1) sliceLocationSensor(1)],ylim,'-k','LineWidth',2)
+    plot([sliceLocationSensor(2) sliceLocationSensor(2)],ylim,'-k','LineWidth',2)
+    title('Slice Position of Sensor Data')
+    hold off
+
+    subplot(2,1,2)
+    hold on
+    plot(x1,magPos,'--c')
+    plot([sliceLocationVideo(1) sliceLocationVideo(1)],ylim,'-k','LineWidth',2)
+    plot([sliceLocationVideo(2) sliceLocationVideo(2)],ylim,'-k','LineWidth',2)
+    title('Slice Position of Video Data')
+    hold off
+
+    % Get information from the user
+    x = input('Are cut locations good (1 0)? ');
+
+    if x == 1
+      break
+    else
+      fixSensorLow = input('Adjust sensor low by: ');
+      fixSensorHigh = input('Adjust sensor high by: ');
+
+      fixVideoLow = input('Adjust video low by: ');
+      fixVideoHigh = input('Adjust video high by: ');
+    end
+
+    % Adjust the data as required
+    sliceLocationSensor(1) = sliceLocationSensor(1) + fixSensorLow;
+    sliceLocationSensor(2) = sliceLocationSensor(2) + fixSensorHigh;
+
+    sliceLocationVideo(1) = sliceLocationVideo(1) + fixVideoLow;
+    sliceLocationVideo(2) = sliceLocationVideo(2) + fixVideoHigh;
+  end
 
   % Output sliced a w and t
-  a.x(1:sliceLocation(1)) = [];
-  a.y(1:sliceLocation(1)) = [];
-  a.z(1:sliceLocation(1)) = [];
-  w.x(1:sliceLocation(1)) = [];
-  w.y(1:sliceLocation(1)) = [];
-  w.z(1:sliceLocation(1)) = [];
-  t.microSeconds(1:sliceLocation(1)) = [];
-  t.seconds(1:sliceLocation(1)) = [];
-  t.frequency(1:sliceLocation(1)) = [];
+  a.x = a.x(sliceLocationSensor(1):sliceLocationSensor(2));
+  a.y = a.y(sliceLocationSensor(1):sliceLocationSensor(2));
+  a.z = a.z(sliceLocationSensor(1):sliceLocationSensor(2));
+  w.x = w.x(sliceLocationSensor(1):sliceLocationSensor(2));
+  w.y = w.y(sliceLocationSensor(1):sliceLocationSensor(2));
+  w.z = w.z(sliceLocationSensor(1):sliceLocationSensor(2));
+  t.microSeconds = t.microSeconds(sliceLocationSensor(1):sliceLocationSensor(2));
+  t.seconds = t.seconds(sliceLocationSensor(1):sliceLocationSensor(2));
+  t.frequency = t.frequency(sliceLocationSensor(1):sliceLocationSensor(2));
 
   % Output sliced pos
-  pos.x(1:sliceLocation(2)) = [];
-  pos.y(1:sliceLocation(2)) = [];
+  pos.x = pos.x(sliceLocationVideo(1):sliceLocationVideo(2));
+  pos.y = pos.y(sliceLocationVideo(1):sliceLocationVideo(2));
 
 end
 
 
-function [sliceLocation] = sliceSensor(mag, extraPoints)
+function [sliceLocation] = slicer(mag)
   % Initialize variables
-  counter = 0;
-  forward = movmean(mag,[0, 2]);
-  back = movmean(mag,[2, 0]);
+  countForward = 0;
+  countBackward = 0;
+  flatForward = mean(mag(1:100));
+  flatBackward = mean(mag(end-100:end));
 
-  % Find the point to cut at
+  % Find the front point to cut at
   for j = 1:length(mag)
-    if abs((forward(j) - back(j)) / (back(j))) > 0.1
+    if abs( (mag(j) - flatForward) ) > 5
       break
     end
-    counter = counter + 1;
+    countForward = countForward + 1;
   end
 
-  sliceLocation = counter - extraPoints;
-end
-
-
-function [sliceLocation] = sliceVideo(mag, extraPoints)
-  % Initialize variables
-  counter = 0;
-  flat = mean(mag(1:150));
-
-  % Find the point to cut at
-  for j = 1:length(mag)
-    if abs((mag(j) - flat) / (flat)) > 0.005
+  % Find the rear point to cut at
+  for j = length(mag):-1:1
+    if abs( (mag(j) - flatBackward) ) > 5
       break
     end
-    counter = counter + 1;
+    countBackward = countBackward + 1;
   end
 
-  sliceLocation = counter - extraPoints;
+  sliceLocation(1) = countForward;
+  sliceLocation(2) = length(mag) - countBackward;
 end
