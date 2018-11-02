b74f4e16a1bbacd2c6ef0d411e8f66e75247ab55
Mark Sherstan <mshersta@ualberta.ca>
2018-11-02 13:45:13 -0600

    Start cleaning the data better.

--- MATLAB_DAQ/main.m
+++ MATLAB_DAQ/main.m
@@ -3,8 +3,8 @@ close all
 clc
 
 % Constant values / variables
-videoFileName = 'Vehicle_Test_1.mp4';
-dataFileName = 'Vehicle_Test_1.csv';
+videoFileName = 'DATA00.mp4';
+dataFileName = 'DATA00.csv';
 extraPoints = 0;
 initialCaptureRate = 240;
 
@@ -14,9 +14,16 @@ initialCaptureRate = 240;
 % Calculate positions
 pos = positionLogger(videoFileName);
 
-% Read in sensor data and clean where required
+% Read in sensor data
 [a,w,t,PWM] = fileReader(dataFileName);
-a.y = cleanUpFreq(a.y,mean(t.frequency),4);
+
+% Clean up the data
+a.xx = cleanUpFreq(a.x,mean(t.frequency),0.5);
+a.yy = cleanUpFreq(a.y,mean(t.frequency),0.5);
+a.zz = cleanUpFreq(a.z,mean(t.frequency),0.5);
+w.xx = movmean(w.x,25);
+w.yy = movmean(w.y,25);
+w.zz = movmean(w.z,25);
 
 % Match up data
 [a,w,t,pos] = dataSlicer(a,w,t,pos);
