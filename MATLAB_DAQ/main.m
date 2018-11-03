d5caa789e012e4f94e950d2d191146a45178042e
MarkSherstan <mshersta@ualberta.ca>
2018-11-03 10:14:17 -0600

    Update with cleanData function

--- MATLAB_DAQ/main.m
+++ MATLAB_DAQ/main.m
@@ -5,7 +5,6 @@ clc
 % Constant values / variables
 videoFileName = 'DATA00.mp4';
 dataFileName = 'DATA00.csv';
-extraPoints = 0;
 initialCaptureRate = 240;
 
 % Get scale
@@ -18,19 +17,14 @@ pos = positionLogger(videoFileName);
 [a,w,t,PWM] = fileReader(dataFileName);
 
 % Clean up the data
-a.xx = cleanUpFreq(a.x,mean(t.frequency),0.5);
-a.yy = cleanUpFreq(a.y,mean(t.frequency),0.5);
-a.zz = cleanUpFreq(a.z,mean(t.frequency),0.5);
-w.xx = movmean(w.x,25);
-w.yy = movmean(w.y,25);
-w.zz = movmean(w.z,25);
+[aa,ww] = cleanData(a,w,t)
 
 % Match up data
-[a,w,t,pos] = dataSlicer(a,w,t,pos);
+[a,w,t,pos] = dataSlicer(aa,ww,t,pos);
 
 % Calculate angles and velocitys
 [angleDeltaRad, angleRad, y] = angleVelCalc(pos, 20, initialCaptureRate);
 
 % Plot the results
 close all
-plotter(angleDeltaRad, angleRad, pos, a, w, t, y, initialCaptureRate)
+plotter(angleDeltaRad, angleRad, pos, aa, ww, t, y, initialCaptureRate)
