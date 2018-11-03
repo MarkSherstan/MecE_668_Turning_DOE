d38462b1f2a0ff46bc608df2fcb79f9499022710
Mark Sherstan <mshersta@ualberta.ca>
2018-11-03 09:18:41 -0600

    Reorder and add in cleanA

--- MATLAB_DAQ/cleanData.m
+++ MATLAB_DAQ/cleanData.m
@@ -1,9 +1,116 @@
-function [dataOut] = cleanData(a,w,t)
+function [a,w] = cleanData(a,w,t)
 
-  w = cleanW(w,t);
   a = cleanA(a,t);
+  w = cleanW(w,t);
 
 end
+
+
+
+function [dataOut] = cleanA(a,t)
+
+  % Set up starting variables
+  Fs = mean(t.frequency);
+  T = 1/Fs;
+  L = length(t.seconds);
+  t = (0:L-1)*T;
+
+  % Plot original data in time domain
+  figure(1)
+  title('Time Domain Plot')
+  xlabel('time (s)')
+  ylabel('magnitude')
+
+  subplot(3,1,1)
+  plot(t.seconds,a.x)
+
+  subplot(3,1,2)
+  plot(t.seconds,a.y)
+
+  subplot(3,1,3)
+  plot(t.seconds,a.z)
+
+  % Perfom FFT
+  Y.x = fft(a.x);
+  P2.x = abs(Y.x/L);
+  P1.x = P2.x(1:L/2+1);
+  P1.x(2:end-1) = 2*P1.x(2:end-1);
+  f.x = Fs*(0:(L/2))/L;
+
+  Y.y = fft(a.y);
+  P2.y = abs(Y.y/L);
+  P1.y = P2.y(1:L/2+1);
+  P1.y(2:end-1) = 2*P1.y(2:end-1);
+  f.y = Fs*(0:(L/2))/L;
+
+  Y.z = fft(a.z);
+  P2.z = abs(Y.z/L);
+  P1.z = P2.z(1:L/2+1);
+  P1.z(2:end-1) = 2*P1.z(2:end-1);
+  f.z = Fs*(0:(L/2))/L;
+
+  % Plot frequency domain
+  figure(2)
+  title('Frequency Domain Plot')
+  xlabel('f (Hz)')
+  ylabel('|P1(f)|')
+
+  subplot(3,1,1)
+  plot(f.x,P1.x)
+
+  subplot(3,1,2)
+  plot(f.y,P1.y)
+
+  subplot(3,1,3)
+  plot(f.z,P1.z)
+
+  x = 0;
+  while (x ~= 1)
+    % Get information from the user
+    x = input('Happy with the data? (1 0)? ');
+
+    if x == 1
+      break
+    else
+      cutOffFreq.x = input('Cut off frequency x: ');
+      cutOffFreq.y = input('Cut off frequency y: ');
+      cutOffFreq.z = input('Cut off frequency z: ');
+    end
+
+    % Design a 6th order butterworth filter based off the frequnecy domain plot
+    fc.x = cutOffFreq.x;
+    fc.y = cutOffFreq.y;
+    fc.z = cutOffFreq.z;
+
+    fs = freq;
+
+    [B,A] = butter(6,fc.x/(fs/2));
+    dataOut.x = filter(B,A,a.x);
+
+    [B,A] = butter(6,fc.y/(fs/2));
+    dataOut.y = filter(B,A,a.y);
+
+    [B,A] = butter(6,fc.z/(fs/2));
+    dataOut.z = filter(B,A,a.z);
+
+    % Plot the filtered data back in the time domain
+    figure(3)
+    title('Time Domain Plot - Filtered')
+    xlabel('time (s)')
+    ylabel('magnitude')
+
+    subplot(3,1,1)
+    plot(t.seconds,dataOut.x)
+
+    subplot(3,1,2)
+    plot(t.seconds,dataOut.y)
+
+    subplot(3,1,3)
+    plot(t.seconds,dataOut.z)
+  end
+
+end
+
 
 
 function [dataOut] = cleanW(w,t)
