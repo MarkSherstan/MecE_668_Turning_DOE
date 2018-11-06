6a9e9cf5076aaf6fbe8528f0c3c881426c790619
MarkSherstan <mshersta@ualberta.ca>
2018-11-05 20:43:08 -0700

    Update fit function to include x value

--- MATLAB_DAQ/createFit.m
+++ MATLAB_DAQ/createFit.m
@@ -1,14 +1,14 @@
-function [fitresult] = createFit(wz)
+function [fitresult] = createFit(tsec, wz)
 
   % Fit
-  [xData, yData] = prepareCurveData( [], wwzz );
+  [xData, yData] = prepareCurveData(tsec, wz);
 
   % Set up fittype and options.
   ft = fittype( 'gauss4' );
   opts = fitoptions( 'Method', 'NonlinearLeastSquares' );
   opts.Display = 'Off';
   opts.Lower = [-Inf -Inf 0 -Inf -Inf 0 -Inf -Inf 0 -Inf -Inf 0];
-  opts.StartPoint = [132.75846094 1773 134.923708659272 126.699814427146 1522 117.962724531341 117.840932648423 1184 92.018311965428 106.847239922242 1011 99.8384694087704];
+  opts.StartPoint = [132.75846094 48.190524 3.09127421153934 125.952347706911 42.578472 2.75295418111384 117.864270359238 34.167132 2.09303822923094 106.249674936169 30.322236 2.33924603899523];
 
   % Fit model to data.
   [fitresult, gof] = fit( xData, yData, ft, opts );
