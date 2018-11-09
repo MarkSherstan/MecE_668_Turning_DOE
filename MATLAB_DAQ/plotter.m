665f1db2eff4427ef5b998f6c8f57da077f3c543
MarkSherstan <mshersta@ualberta.ca>
2018-11-08 21:23:38 -0700

    Added centered 0 plot, removed curve fitted plots, added instant v plot

--- MATLAB_DAQ/plotter.m
+++ MATLAB_DAQ/plotter.m
@@ -1,40 +1,53 @@
-function [] = plotter(pos, a, w, t, y)
+function [] = plotter(pos,posZerod,radius,a,w,t)
 
-figure(1)
+figure
 plot(pos.x,pos.y)
 title('Position Plot')
-xlabel('x position [cm]')
-ylabel('y position [cm]')
+xlabel('x position [pixel]')
+ylabel('y position [pixel]')
 axis equal
 
 
 
-figure(2)
-% This is where some plot will go with the center points
+figure
+plot(posZerod.x,posZerod.y)
+title('Position Plot with Circles Centered at Zero')
+xlabel('x position [pixel]')
+ylabel('y position [pixel]')
+axis equal
 
 
 
-figure(3)
+figure
+r = resample(radius.meters,length(t.seconds),length(radius.meters));
+plot(t.seconds,r)
+title('Circle Radius as a function of Frame Number')
+xlabel('Time [s]')
+ylabel('Radius of Circle [m]')
+
+
+
+figure
 plot(t.seconds,a.x,t.seconds,a.y,t.seconds,a.z)
 title('Acceleration as a function of Time')
 legend('a_x','a_y','a_z')
-xlabel('Time (s)')
+xlabel('Time [s]')
 ylabel('Acceleration [g]')
 
 
 
-figure(4)
+figure
 plot(t.seconds,w.x,t.seconds,w.y,t.seconds,w.z)
 title('Angular Velocity as a function of Time')
 legend('w_x','w_y','w_z')
-xlabel('Time (s)')
+xlabel('Time [s]')
 ylabel('Angular Velocity [deg/s]')
 
 
 
-figure(5)
+figure
 title('Acceleration and Angular Velocity as a function of Time')
-xlabel('Time (s)')
+xlabel('Time [s]')
 
 yyaxis left
 leftA = plot(t.seconds,a.x,'--r',t.seconds,a.y,'--g',t.seconds,a.z,'--b');
@@ -48,26 +61,9 @@ Leg = legend([leftA; rightW], {'a_x','a_y','a_z','w_x','w_y','w_z'});
 
 
 
-figure(6)
-yyaxis left
-leftA = plot(t.seconds,a.x,'-r',t.seconds,a.y,'-g',t.seconds,a.z,'-b');
-ylabel('Acceleration [g]')
-
-yyaxis right
-rightW = plot(y,'-k');
-ylabel('Angular Velocity')
-
-Leg = legend([leftA; rightW], {'a_x','a_y','a_z','w'});
-xlabel('Time (s)')
-
-
-
-figure(7)
-hold on
-  plot(t.seconds,w.x,'-r',t.seconds,w.y,'-g',t.seconds,w.z,'-b');
-  plot(y,'-k')
-hold off
-
-xlabel('Time (s)')
-ylabel('Angular Velocity Sensor [deg/s]')
-legend('w_x','w_y','w_z')
+figure
+instantV = r.*w.z;
+plot(t.seconds,instantV)
+title('Instantaneous velocity as a function of Time')
+xlabel('Time [s]')
+ylabel('Instantaneous Velocity [m/s]')
