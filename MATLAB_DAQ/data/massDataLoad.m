85f1de3bec05fd84945ec5390590a98900111bf4
MarkSherstan <mshersta@ualberta.ca>
2018-11-19 23:28:08 -0700

    Wrote quick script to merge all the results onto a handful of plots

--- MATLAB_DAQ/data/massDataLoad.m
+++ MATLAB_DAQ/data/massDataLoad.m
@@ -2,18 +2,94 @@ clear all
 close all
 clc
 
-figure(1)
-str = cell(3,1);
-
-for ii = 1:3
+for ii = 1:16
     str{ii} = strcat('run',num2str(ii),'.mat');
     load(str{ii})
-    
-    
-    hold on 
-    r = resample(radius.meters,length(t.seconds),length(radius.meters));
-    plot(t.seconds,r)
+
+    hold on
+    figure(1)
+    hold on
+    r = resample(radius.meters,length(tt.seconds),length(radius.meters));
+    plot(tt.seconds,r)
+    title('Circle Radius as a function of Time')
+    xlabel('Time [s]')
+    ylabel('Radius of Circle [m]')
+    legend(str')
+    hold off
+
+    figure(2)
+    hold on
+    plot(tt.seconds,ww.z)
+    title('Angular velocity as a function of Time')
+    xlabel('Time [s]')
+    ylabel('Angular velocity [deg/s]')
+    legend(str')
+    hold off
+
+    figure(3)
+    hold on
+    plot(posZerod.x,posZerod.y)
+    title('Position Plot with Circles Centered at Zero')
+    xlabel('x position [pixel]')
+    ylabel('y position [pixel]')
+    axis equal
+    legend(str')
+    hold off
+
+    figure(4)
+    hold on
+    instantV = r.*ww.z*0.0174533;
+    plot(tt.seconds,instantV)
+    title('Instantaneous velocity as a function of Time')
+    xlabel('Time [s]')
+    ylabel('Instantaneous Velocity [m/s]')
+    legend(str')
+    hold off
 end
 
-legend(str')
-hold off
+clear str
+
+for ii = 1:3
+    str{ii} = strcat('midPoint',num2str(ii),'.mat');
+    load(str{ii})
+
+    hold on
+    figure(5)
+    hold on
+    r = resample(radius.meters,length(tt.seconds),length(radius.meters));
+    plot(tt.seconds,r)
+    title('Circle Radius as a function of Time - Center Point')
+    xlabel('Time [s]')
+    ylabel('Radius of Circle [m]')
+    legend(str')
+    hold off
+
+    figure(6)
+    hold on
+    plot(tt.seconds,ww.z)
+    title('Angular velocity as a function of Time - Center Point')
+    xlabel('Time [s]')
+    ylabel('Angular velocity [deg/s]')
+    legend(str')
+    hold off
+
+    figure(7)
+    hold on
+    plot(posZerod.x,posZerod.y)
+    title('Position Plot with Circles Centered at Zero - Center Point')
+    xlabel('x position [pixel]')
+    ylabel('y position [pixel]')
+    axis equal
+    legend(str')
+    hold off
+
+    figure(8)
+    hold on
+    instantV = r.*ww.z*0.0174533;
+    plot(tt.seconds,instantV)
+    title('Instantaneous velocity as a function of Time - Center Point')
+    xlabel('Time [s]')
+    ylabel('Instantaneous Velocity [m/s]')
+    legend(str')
+    hold off
+end
